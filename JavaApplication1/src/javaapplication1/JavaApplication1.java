afc63ed6e0a496d2728240c486a7a96e1fee4f2d
shavaizknz98 <shavaizknz98@gmail.com>
2019-02-10 14:42:59 +0400

    Initial Commit

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,17 @@
+package javaapplication1;
+
+public class JavaApplication1 {
+
+    public static void addFeatureA()
+    {
+        System.out.println("Feature A added");
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JavaApplication1 J = new JavaApplication1();
+        addFeatureA();
+        //this.addFeatureA();
+        
+    }
+    
+}
